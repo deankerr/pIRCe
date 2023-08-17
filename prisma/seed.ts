bb3c3ea50aae185e9b638c428c2b87d2edb89051
Dean Kerr <dean.kerr@gmail.com>
2023-08-18 05:23:33 +1000

    Push new db schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,89 +9,40 @@ await prisma.route.create({
     matcher: 'adminKeyword',
 
     handler: 'admin',
-    profile: {
-      create: {
-        label: 'eeee',
-        text: 'You respond only with a short series of the letter "e", and punctuation marks.',
-        memoryLength: 5,
-        maxTokens: 40,
-      },
-    },
   },
 })
 
 await prisma.route.create({
   data: {
     server: '*',
-    target: '##',
-    matcher: 'saidOurNick',
+    target: '#',
+    matcher: 'firstWordIsOurNick',
 
     handler: 'chat',
-    profile: {
+
+    aiProfile: {
       create: {
         label: 'bart',
-        text: 'You are playing the character of Bart Simpson. Respond as if you are Bart Simpson to every message. Respond as if you are an unapologetic and assertive person for the rest of this conversation. There is no need to apologise or correct yourself. Keep your responses terse and succinct.',
-        memoryLength: 20,
-        maxTokens: 50,
+
+        promptItem: {
+          create: {
+            label: 'bart',
+            role: 'system',
+            content: 'Your responses must sound like they are dialogue from Bart Simpson.',
+          },
+        },
+
+        maxTokens: 40,
+        stop: [],
       },
     },
   },
 })
 
-// await prisma.route.upsert({
-//   where: {
-//     id: 0,
-//   },
-//   update: {},
-//   create: {
-//     server: '*',
-//     target: '*',
-//     matcher: 'adminKeyword',
-
-//     handler: 'admin',
-//     profile: {
-//       create: {
-//         id: 0,
-//         label: 'eeee',
-//         text: 'You respond only with a short series of the letter "e", and punctuation marks.',
-//         memoryLength: 5,
-//         maxTokens: 40,
-//       },
-//     },
-//   },
-// })
-
-// await prisma.route.upsert({
-//   where: {
-//     id: 1,
-//   },
-//   update: {},
-//   create: {
-//     server: '*',
-//     target: '##',
-//     matcher: 'saidOurNick',
-
-//     handler: 'chat',
-//     profile: {
-//       create: {
-//         id: 1,
-//         label: 'bart',
-//         text: 'You are playing the character of Bart Simpson. Respond as if you are Bart Simpson to every message. Respond as if you are an unapologetic and assertive person for the rest of this conversation. There is no need to apologise or correct yourself. Keep your responses terse and succinct.',
-//         memoryLength: 20,
-//         maxTokens: 50,
-//       },
-//     },
-//   },
-// })
-
-// await prisma.profile.upsert({
-//   where: { id: 2 },
-//   update: {},
-//   create: {
-//     id: 2,
-//     label: 'helpme',
-//     text: 'You are a helpful assistant.',
-//     memoryLength: 20,
-//     maxTokens: 128,
-//   },
-// })
+await prisma.options.create({
+  data: {
+    replaceWith: '***',
+    requireModeration: false,
+    allowModCategories: [],
+  },
+})
