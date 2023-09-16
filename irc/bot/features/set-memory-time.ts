260694a2d482091b0a634f1e1b85e6d5b0be1c2a
Dean Kerr <dean.kerr@gmail.com>
2023-09-17 01:37:22 +1000

    add setMemoryTime feedback

--- irc/bot/features/set-memory-time.ts
+++ irc/bot/features/set-memory-time.ts
@@ -1,10 +1,12 @@
 import type { InitialContext } from '../types.js'
 import debug from 'debug'
 import { setMessageMaxHistoryTimeAllNow } from '../lib/db.js'
+import { respond } from '../send.js'
 
 const log = debug('pIRCe:user-command')
 
-export async function setMemoryTime(_event: InitialContext) {
+export async function setMemoryTime(ctx: InitialContext) {
   await setMessageMaxHistoryTimeAllNow()
   log('max message history time updated')
+  await respond.action(ctx, 'stares vacantly for a few moments')
 }
